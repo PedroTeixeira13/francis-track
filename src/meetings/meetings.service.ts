4f28bf9a8ba58d77baf188f92a7027eb8815c793
Pedro Teixeira <pedrohcrt@gmail.com>
2024-02-08 16:18:39 -0300

    fix: fixing circular structure

--- src/meetings/meetings.service.ts
+++ src/meetings/meetings.service.ts
@@ -1,12 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { CustomersService } from 'src/customers/customers.service';
 import { RoomsService } from 'src/rooms/rooms.service';
 import { UsersMeetingsService } from 'src/users-meetings/users-meetings.service';
 import { UsersService } from 'src/users/users.service';
-import { LessThan, MoreThan, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CreateMeetingDto } from './dtos/create-meeting.dto';
 import { Meeting } from './meeting.entity';
-import { CustomersService } from 'src/customers/customers.service';
 const { parseJSON } = require('date-fns');
 
 @Injectable()
@@ -36,10 +36,11 @@ export class MeetingsService {
   }
 
   async create(body: CreateMeetingDto, applicantId: string) {
+    
     const newMeeting = await this.repo.create();
-
+    
     const { roomName, users, customer, subject, startTime, endTime } = body;
-
+    
     newMeeting.subject = subject;
     newMeeting.applicant = await this.usersService.findById(applicantId);
     newMeeting.room = await this.roomsService.findRoom(roomName);
@@ -48,15 +49,14 @@ export class MeetingsService {
       users,
       newMeeting,
     );
-
     
-
     const utcStartTime = parseJSON(startTime);
     newMeeting.startTime = utcStartTime;
-
+    
     const utcEndTime = parseJSON(endTime);
     newMeeting.endTime = utcEndTime;
-
+    
+    delete newMeeting.users.meetings;
     return this.repo.save(newMeeting);
   }
 
